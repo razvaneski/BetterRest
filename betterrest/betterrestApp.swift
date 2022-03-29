dfb4f4a6f9e49488ab151f59621f6e755bcb0503
razvaneski <razvan@dumitriu.co.uk>
2022-03-29 17:17:02 +0300

    Initial Commit

---
+++ betterrest/betterrestApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterrestApp.swift
+//  betterrest
+//
+//  Created by Razvan Dumitriu on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct betterrestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
